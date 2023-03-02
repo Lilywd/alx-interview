17427847965e7718a0f87d53b64a189159be7f2e
Lilywd <ndungulilianwanjiku@gmail.com>
2023-03-03 00:27:51 +0300

    grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
-"""
-grid
-"""
+""" Island Perimeter """
+
+
 def island_perimeter(grid):
-    perimeter = 0
-    height = len(grid)
-    width = len(grid[0])
-    
-    for row in range(height):
-        for col in range(width):
-            if grid[row][col] == 1:
-                # if the cell is land, add its perimeter
-                perimeter += 4
-                # check if there is land to the left, and if so subtract 2 from the perimeter
-                if col > 0 and grid[row][col-1] == 1:
-                    perimeter -= 2
-                # check if there is land above, and if so subtract 2 from the perimeter
-                if row > 0 and grid[row-1][col] == 1:
-                    perimeter -= 2
-    return perimeter
+    """ return the perimeter of the island described in grid """
+    total_perimeter = 0
+
+    for i, row in enumerate(grid):
+        for j, element in enumerate(row):
+            # Check if element is land or sea
+            if (element == 0):
+                continue
+
+            # Left check
+            if (j != 0 and row[j - 1] == 0):
+                total_perimeter += 1
+            if (j == 0):
+                # left edge case
+                total_perimeter += 1
+
+            # Right check
+            if (j != len(row) - 1 and row[j + 1] == 0):
+                total_perimeter += 1
+            if (j == len(row) - 1):
+                # right edge case
+                total_perimeter += 1
+
+            # Upper check
+            if (i != 0 and grid[i - 1][j] == 0):
+                total_perimeter += 1
+            if (i == 0):
+                # top edge case
+                total_perimeter += 1
+
+            # Bottom Check
+            if (i != len(grid) - 1 and grid[i + 1][j] == 0):
+                total_perimeter += 1
+            if (i == len(grid) - 1):
+                # bottom edge case
+                total_perimeter += 1
+
+    return total_perimeter
